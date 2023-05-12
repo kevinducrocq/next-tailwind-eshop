1b8c13368a000b42740cbb3cb93ebd8090336d8e
kevinducrocq <kevin.ducrocq@live.fr>
2023-05-12 11:11:11 +0200

    page commande modifs

--- pages/order/[id].js
+++ pages/order/[id].js
@@ -25,6 +25,8 @@ function ProductPage() {
     });
   }, [id, router]);
 
+  console.log(order);
+
   if (order.length === 0) {
     return <div>Commande non trouvée</div>;
   }
@@ -37,42 +39,60 @@ function ProductPage() {
 
       <div className='grid md:grid-cols-4 md:gap-5'>
         <div className='overflow-x-auto md:col-span-3'>
-          <div className='card  p-5'>
-            <h2 className='mb-2 text-lg'>Shipping Address</h2>
-            <div>
-              {order.shippingAddress?.firstName},{" "}
-              {order.shippingAddress?.address}, {order.shippingAddress?.city},{" "}
-              {order.shippingAddress?.postalCode},{" "}
-              {order.shippingAddress?.country}
+          <div className='card p-5'>
+            <div className='grid md:grid-cols-4 md:gap-5'>
+              <div className='overflow-x-auto md:col-span-2'>
+                <h2 className='mb-2 text-lg'>Adresse de livraison</h2>
+                <hr />
+                <div className='mb-2 mt-2'>
+                  {order.shippingAddress?.firstName}&nbsp;
+                  {order.shippingAddress?.lastName} <br />
+                  {order.shippingAddress?.address} <br />{" "}
+                  {order.shippingAddress?.city} <br />{" "}
+                  {order.shippingAddress?.postalCode}{" "}
+                  {order.shippingAddress?.country}
+                </div>
+              </div>
+              <div className='overflow-x-auto md:col-span-2'>
+                <h2 className='mb-2 text-lg'>Adresse de facturation</h2>
+                <hr />
+                <div className='mb-2 mt-2'>
+                  {order.billingAddress?.firstName}{" "}
+                  {order.billingAddress?.lastName} <br />
+                  {order.billingAddress?.address} <br />{" "}
+                  {order.billingAddress?.city} <br />{" "}
+                  {order.billingAddress?.postalCode}{" "}
+                  {order.billingAddress?.country}
+                </div>{" "}
+              </div>
             </div>
             {order.isDelivered ? (
               <div className='alert-success'>
-                Delivered at {order.deliveredAt}
+                Livré le : {order.deliveredAt}
               </div>
             ) : (
-              <div className='alert-error'>Not delivered</div>
+              <div className='alert-error'>Pas encore livré</div>
             )}
           </div>
 
           <div className='card p-5'>
-            <h2 className='mb-2 text-lg'>Payment Method</h2>
+            <h2 className='mb-2 text-lg'>Méthode de paiement</h2> <hr />
             <div>{order.paymentMethod}</div>
             {order.isPaid ? (
               <div className='alert-success'>Paid at {order.paidAt}</div>
             ) : (
-              <div className='alert-error'>Not paid</div>
+              <div className='alert-error'>Pas encore payé</div>
             )}
           </div>
-
           <div className='card overflow-x-auto p-5'>
-            <h2 className='mb-2 text-lg'>Order Items</h2>
+            <h2 className='mb-2 text-lg'>Produits</h2>
             <table className='min-w-full'>
               <thead className='border-b'>
                 <tr>
-                  <th className='px-5 text-left'>Item</th>
-                  <th className='    p-5 text-right'>Quantity</th>
-                  <th className='  p-5 text-right'>Price</th>
-                  <th className='p-5 text-right'>Subtotal</th>
+                  <th className='px-5 text-left'>Produit</th>
+                  <th className='    p-5 text-right'>Quantité</th>
+                  <th className='  p-5 text-right'>Prix</th>
+                  <th className='p-5 text-right'>Sous-total</th>
                 </tr>
               </thead>
               <tbody>
@@ -105,30 +125,30 @@ function ProductPage() {
         </div>
         <div>
           <div className='card  p-5'>
-            <h2 className='mb-2 text-lg'>Order Summary</h2>
+            <h2 className='mb-2 text-lg'>Résumé de la commande</h2>
             <ul>
               <li>
                 <div className='mb-2 flex justify-between'>
-                  <div>Items</div>
-                  <div>€{order.itemsPrice}</div>
+                  <div>Produits</div>
+                  <div>{order.itemsPrice} &euro;</div>
                 </div>
               </li>{" "}
               <li>
                 <div className='mb-2 flex justify-between'>
-                  <div>Tax</div>
-                  <div>€{order.taxPrice}</div>
+                  <div>Taxes</div>
+                  <div>{order.taxPrice} &euro;</div>
                 </div>
               </li>
               <li>
                 <div className='mb-2 flex justify-between'>
-                  <div>Shipping</div>
-                  <div>€{order.shippingPrice}</div>
+                  <div>Livraison</div>
+                  <div>{order.shippingPrice} &euro;</div>
                 </div>
               </li>
               <li>
                 <div className='mb-2 flex justify-between'>
                   <div>Total</div>
-                  <div>€{order.totalPrice}</div>
+                  <div>{order.totalPrice} &euro;</div>
                 </div>
               </li>
             </ul>
@@ -140,4 +160,5 @@ function ProductPage() {
 }
 
 ProductPage.auth = true;
+
 export default ProductPage;
