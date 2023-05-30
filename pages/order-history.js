cd140073cf1b58743655ff7f4f5190c21883f531
kevinducrocq <kevin.ducrocq@live.fr>
2023-05-30 19:34:52 +0200

    responsive carousel

--- pages/order-history.js
+++ pages/order-history.js
@@ -4,6 +4,8 @@ import fetchUserOrders from "@/domain/order/fetchUserOrders";
 import Image from "next/image";
 import Link from "next/link";
 import React, { useEffect, useState } from "react";
+import { saveAs } from "file-saver";
+import { PDFDocument, rgb, StandardFonts } from "pdf-lib";
 
 export default function OrderHistoryPage() {
   const [orders, setOrders] = useState([]);
@@ -12,74 +14,153 @@ export default function OrderHistoryPage() {
     fetchUserOrders(setOrders);
   }, []);
 
+  const generateInvoicePDF = async (order) => {
+    try {
+      const pdfDoc = await PDFDocument.create();
+      const page = pdfDoc.addPage();
+
+      const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman);
+
+      const { width, height } = page.getSize();
+      const margin = 50;
+      const fontSize = 12;
+      const lineHeight = fontSize * 1.5;
+
+      let y = height - margin;
+
+      // Ajouter le titre de la facture
+      page.drawText("Facture", {
+        x: margin,
+        y,
+        size: 20,
+        font: timesRomanFont,
+        color: rgb(0, 0, 0),
+      });
+
+      // Ajouter les détails de la facture
+      y -= lineHeight;
+      page.drawText(`Commande n°: ${order.id}`, {
+        x: margin,
+        y,
+        size: fontSize,
+        font: timesRomanFont,
+        color: rgb(0, 0, 0),
+      });
+
+      // Ajouter les informations sur la commande
+      y -= lineHeight;
+      page.drawText(`Créée le : ${order.createdAt.substring(0, 10)}`, {
+        x: margin,
+        y,
+        size: fontSize,
+        font: timesRomanFont,
+        color: rgb(0, 0, 0),
+      });
+
+      // Ajouter d'autres détails de la facture...
+
+      // Génération du fichier PDF
+      const pdfBytes = await pdfDoc.save();
+
+      // Enregistrement et téléchargement du fichier PDF
+      const blob = new Blob([pdfBytes], { type: "application/pdf" });
+      saveAs(blob, "facture.pdf");
+    } catch (error) {
+      console.error(
+        "Une erreur s'est produite lors de la génération de la facture PDF :",
+        error
+      );
+    }
+  };
+
   return (
     <Layout title='Historique de commandes'>
-      <h1>Historique de commandes</h1>
-      {orders?.map((order) => {
-        return (
-          <div className='card p-5' key={order.id}>
-            <div className='flex justify-between'>
-              <ul>
-                <li>Commande n°: {order.id}</li>
-                <li>Créée le : {order.createdAt.substring(0, 10)}</li>
-                <li>{order.isPaid ? "Payée" : "En attente de paiement"}</li>
-                <li>
+      <div className='container mx-auto px-4 py-8'>
+        <h1 className='text-3xl font-semibold mb-4'>Historique de commandes</h1>
+        {orders?.map((order) => (
+          <div
+            className='bg-white shadow-md rounded-md p-6 mb-6'
+            key={order.id}
+          >
+            <div className='flex flex-col sm:flex-row items-start justify-between mb-6'>
+              <div>
+                <h2 className='text-lg font-semibold'>
+                  Commande n°: {order.id}
+                </h2>
+                <p className='text-sm'>
+                  Créée le : {order.createdAt.substring(0, 10)}
+                </p>
+                <p className='text-sm'>
+                  {order.isPaid ? "Payée" : "En attente de paiement"}
+                </p>
+                <p className='text-sm'>
                   {order.isDelivered
-                    ? "Envoyée le" + order.deliveredAt.substring(0, 10)
+                    ? "Envoyée le " + order.deliveredAt.substring(0, 10)
                     : "Pas encore envoyée"}
-                </li>
-                <li>Total : {order.totalPrice} &euro;</li>
-                <li>
+                </p>
+                <p className='text-lg mt-4'>
+                  Total : {order.totalPrice} &euro;
+                </p>
+                <p className='mt-2'>
                   <Link href={`/order/${order.id}`}>Détails</Link>
-                </li>
-              </ul>
-              <div className='my-4 border'>
-                <Carousel
-                  items={order.orderItems.map((item) => (
-                    <div key={item.id} className='flex items-center'>
-                      <div className='mr-4'>
-                        <Image
-                          src={item.image}
-                          alt={item.name}
-                          width={150}
-                          height={150}
-                        />
-                      </div>
-                      <div>
-                        <h3 className='text-lg font-semibold'>{item.name}</h3>
-                        <p className='mb-2'>x {item.quantity}</p>
-                        <p>{item.price} &euro;</p>
-                      </div>
-                    </div>
-                  ))}
-                />
+                </p>
+              </div>
+
+              <div className='mt-4 sm:mt-0 sm:ml-4'>
+                <h3 className='text-lg font-semibold'>Adresse de livraison</h3>
+                <p className='text-sm'>
+                  {order.shippingFirstName} {order.shippingLastName}
+                </p>
+                <p className='text-sm'>{order.shippingStreet}</p>
+                <p className='text-sm'>
+                  {order.shippingZip} {order.shippingCity}
+                </p>
+              </div>
+              <div className='mt-4 sm:mt-0 sm:ml-4'>
+                <h3 className='text-lg font-semibold'>
+                  Adresse de facturation
+                </h3>
+                <p className='text-sm'>
+                  {order.billingFirstName} {order.billingLastName}
+                </p>
+                <p className='text-sm'>{order.billingStreet}</p>
+                <p className='text-sm'>
+                  {order.billingZip} {order.billingCity}
+                </p>
+              </div>
+              <div className='flex items-center mt-8 sm:mt-0'>
+                <button
+                  className='bg-blue-500 hover:bg-blue-700 text-white font-semibold py-2 px-4 rounded'
+                  onClick={() => generateInvoicePDF(order)}
+                >
+                  Facture
+                </button>
               </div>
             </div>
-            <div className='flex justify-around'>
-              <ul>
-                <h2 className='text-xl font-semibold'>Adresse de livraison</h2>
-                <li>{order.shippingFirstName}</li>
-                <li>{order.shippingLastName}</li>
-                <li>{order.shippingStreet}</li>
-                <li>
-                  {order.shippingZip} {order.shippingCity}
-                </li>
-              </ul>
-              <ul>
-                <h2 className='text-xl font-semibold'>
-                  Adresse de facturation
-                </h2>
-                <li>{order.billingFirstName}</li>
-                <li>{order.billingLastName}</li>
-                <li>{order.billingStreet}</li>
-                <li>
-                  {order.billingZip} {order.billingCity}
-                </li>
-              </ul>
+            <div className='mt-8 max-h-40 overflow-y-auto'>
+              <Carousel
+                items={order.orderItems.map((item) => (
+                  <div key={item.id} className='flex items-center'>
+                    <div className='mr-4'>
+                      <Image
+                        src={item.image}
+                        alt={item.name}
+                        width={150}
+                        height={150}
+                      />
+                    </div>
+                    <div>
+                      <h4 className='text-lg font-semibold'>{item.name}</h4>
+                      <p className='text-sm mb-2'>x {item.quantity}</p>
+                      <p className='text-sm'>{item.price} &euro;</p>
+                    </div>
+                  </div>
+                ))}
+              />
             </div>
           </div>
-        );
-      })}
+        ))}
+      </div>
     </Layout>
   );
 }
